833ec77d9fa8c9922c2e917c83d5eadd8bd7e365
Claire Vita <clairej.vita@gmail.com>
2021-01-11 22:37:13 -0500

    data/cache names

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,5 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
@@ -7,3 +9,17 @@ const FILES_TO_CACHE = [
     "/assets/images/icons/icon-192x192.png",
     "/assets/images/icons/icon-512x512.png"
 ]
+
+
+// install
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(staticFilesToPreCache);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
